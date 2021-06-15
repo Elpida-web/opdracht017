a230b5371284878cf3471d0a9c8c86aaf68f949e
Elpida-web <84407349+Elpida-web@users.noreply.github.com>
2021-06-15 11:30:23 +0200

    Add files via upload

---
+++ Opdracht017/script.js
@@ -0,0 +1,21 @@
+// const hundredMore = function (number) {
+// const sum = number > 100;
+// return sum;
+// };
+
+// const age = hundredMore(100);
+// console.log(age);
+
+const enterDecision = function (maxClub, inClub, enterAge) {
+    const number = ("enterAge >= 18");
+    const sum = maxClub + inClub;
+    if (sum > 100) {
+        return "It's too busy now, come back later";
+    }
+    return "Come in";
+};
+
+const age(19);
+const result = enterDecision(9, 6)
+console.log(result);
+
